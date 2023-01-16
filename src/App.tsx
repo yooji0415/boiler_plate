9dd9ac570a8964808da131ecc77c28d5034968b1
yooji0415 <gch05406@gmail.com>
2023-01-16 12:03:31 +0900

    feat: svgr 테스트용 코드 및 이미지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Logo from '../public/react.svg';
+
 const App = () => {
   return (
     <>
+      <Logo />
       <h1>boiler plate for React TypeScript</h1>
       <h1>mode: {process.env.NODE_ENV}</h1>
     </>
